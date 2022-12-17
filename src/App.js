1e18044c071681a5ecb66e557affc2476b862a9f
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-17 17:05:42 +0300

    files changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+//  "formatjs.io" for installation
 
+import "./App.css";
+import { useState, useEffect } from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
+
+const messages = {
+  tr: {
+    title: "Merhaba Dünya",
+    description: "3 yeni mesajınız var.",
+  },
+  en: {
+    title: "Hello World",
+    description: "You have 3 new messages.",
+  },
+};
 function App() {
+  const isLocale = localStorage.getItem("locale");
+  //  default browser language -- (tr)
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+
+  const [locale, setLocale] = useState(defaultLocale);
+
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        {" "}
+        <FormattedMessage id="title" />
+        <br />
+        <br />
+        <FormattedMessage id="description" />
+        <br />
+        <br />
+        <button onClick={() => setLocale("tr")}>TR</button>
+        <button onClick={() => setLocale("en")}>EN</button>
+      </IntlProvider>
     </div>
   );
 }
